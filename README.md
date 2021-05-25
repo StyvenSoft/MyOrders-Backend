28f5909b5a301030ffdc1ce2f16b9c86e639f1c8
Steveen <steveen.echeverri@gmail.com>
2021-05-24 22:35:07 -0500

    Created services app and components formGroup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # MyOrders-Backend App
 
 In this project we create a basic command manager in Spring Boot, even how to create the project in Angular and be able to do all the operations of CRUD API Rest.
+
+### Preview Entity–relationship model DB
+
+![dbDiagram](./src/img/ERDDiagram-MyOrders.jpg)
--- myorders-client/src/app/app.component.ts
+++ myorders-client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomersService } from './services/customers/customers.service';
+import { EmployeesService } from './services/employees/employees.service';
+import { OrdersService } from './services/orders/orders.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myorders-client';
+export class AppComponent implements OnInit{
+  
+  orderForm: FormGroup;
+
+  constructor (
+    public fb: FormBuilder,
+    public customersService: CustomersService,
+    public employeesService: EmployeesService,
+    public ordersService: OrdersService,
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      orderDate : ['', Validators.required],
+      quantity : ['', Validators.required],
+      unitPrice : ['', Validators.required],
+    })
+  }
 }
